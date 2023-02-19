bc239a1a474b30351229664d2d60f7e2485a8272
razasaeed <36797633+razasaeed@users.noreply.github.com>
2023-02-19 09:46:43 -0800

    feature clear functionality (#8)
    * feature clear functionality
    
    * clear icon property

--- OmniEditText/src/main/java/com/omni/input/OmniEditText.kt
+++ OmniEditText/src/main/java/com/omni/input/OmniEditText.kt
@@ -1,15 +1,18 @@
 package com.omni.input
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 
 class OmniEditText @JvmOverloads constructor(
     context: Context, attrs: AttributeSet?
@@ -21,15 +24,21 @@ class OmniEditText @JvmOverloads constructor(
     private var omniHintText: String? = null
     private var omniTextColor = Int.MIN_VALUE
     private var omniHintColor = Int.MIN_VALUE
+    private var omniEnableClearButton = false
+    private var omniClearButtonIcon: Drawable? = null
 
     var editText: EditText
     var errorTextView: TextView
+    var clInput: ConstraintLayout
+    var ivClear: ImageView
 
     init {
         LayoutInflater.from(context).inflate(R.layout.omni_edit_text_layout, this, true)
 
         editText = findViewById<EditText>(R.id.edit_text)
         errorTextView = findViewById<TextView>(R.id.tv_error)
+        clInput = findViewById<ConstraintLayout>(R.id.cl_input)
+        ivClear = findViewById<ImageView>(R.id.iv_clear)
 
         attrs?.let {
             val a = context.obtainStyledAttributes(attrs, R.styleable.OmniEditText)
@@ -38,10 +47,13 @@ class OmniEditText @JvmOverloads constructor(
             omniTextColor = a.getInt(R.styleable.OmniEditText_omniTextColor, Int.MIN_VALUE)
             omniHintColor = a.getInt(R.styleable.OmniEditText_omniHintColor, Int.MIN_VALUE)
             omniHintText = a.getString(R.styleable.OmniEditText_omniHintText)
+            omniEnableClearButton = a.getBoolean(R.styleable.OmniEditText_omniEnableClearButton, false)
+            omniClearButtonIcon = a.getDrawable(R.styleable.OmniEditText_omniClearButtonIcon)
             a.recycle()
         }
 
         setProperties()
+        setClickListeners()
 
         editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -50,6 +62,12 @@ class OmniEditText @JvmOverloads constructor(
 
             override fun afterTextChanged(s: Editable?) {
                 val input = s.toString().toFloatOrNull()
+                if (s.toString().isNotEmpty()) {
+                    ivClear.isVisible = omniEnableClearButton
+                } else {
+                    ivClear.isVisible = false
+                }
+
                 if (omniMaxValue > 0 && omniMinValue < 0) {
                     if (input != null && input > omniMaxValue) {
                         errorMessage = String.format(
@@ -94,10 +112,20 @@ class OmniEditText @JvmOverloads constructor(
         })
     }
 
+    private fun setClickListeners() {
+        ivClear.setOnClickListener {
+            editText.setText("")
+            ivClear.visibility = GONE
+        }
+    }
+
     private fun setProperties() {
-        editText.setBackgroundResource(R.drawable.default_background)
+        if (omniClearButtonIcon != null) {
+            ivClear.setImageDrawable(omniClearButtonIcon)
+        }
+        clInput.setBackgroundResource(R.drawable.default_background)
         val padding = resources.getDimensionPixelSize(R.dimen._10px)
-        editText.setPadding(padding, padding, padding, padding)
+        clInput.setPadding(padding, padding, padding, padding)
         editText.setTextColor(
             when (omniTextColor > 0) {
                 true -> omniTextColor
@@ -116,11 +144,11 @@ class OmniEditText @JvmOverloads constructor(
 
     private fun setError(errorMessage: String?) {
         errorTextView.text = errorMessage
-        editText.setBackgroundResource(R.drawable.error_background)
+        clInput.setBackgroundResource(R.drawable.error_background)
     }
 
     private fun clearError() {
         errorTextView.text = null
-        editText.setBackgroundResource(R.drawable.default_background)
+        clInput.setBackgroundResource(R.drawable.default_background)
     }
 }
